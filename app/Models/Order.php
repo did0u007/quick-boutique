a9767f273d93305fd72ca7fbbdd93b52efae0b7b
Oualid <mr.did0u007@gmail.com>
2023-12-04 15:52:35 +0100

    migration OK

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'code',
+        'user_id',
+        'order_at',
+        'status',
+        'amount',
+        'discount',
+
+    ];
+}
